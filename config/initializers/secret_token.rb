9ad3470e06d0fcfedc17948c30c3c0f8b26445a4
JonZach <Zach@yourgreenpal.com>
2013-05-29 11:53:39 -0500

    generated new rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NSSblog::Application.config.secret_token = 'e8d41b495c07ec8980d47614cb32f9c3f69e1573fda727cdad7b77b0cd836593ba2d6fba66105d579562e368053ad652f86fda2ef8c95a8afa77d423f8113a6c'
