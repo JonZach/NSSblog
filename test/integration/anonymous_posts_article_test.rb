af7f2a26bed96a63603a1a4ddf396bdf3f3df287
JonZach <Zach@yourgreenpal.com>
2013-05-30 11:37:28 -0500

    initialized sqlite3 db and created 1st integration test.

---
+++ test/integration/anonymous_posts_article_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
+    test "view a list of articles" do
+        Post.create!(title: "First Post!!", body: "This is awkward...I have nothing to say :(")
+        Post.create!(title: "Wow, It's been a while!", body: "And yet, I'm still not a write :( :( lolol")
+        visit '/'
+        click_link "View All Posts"
+        assert_current_path '/posts'
+        # assume we have a page variable
+        assert_include page.content, "First Post!!"
+        assert_include page.content, "Wow, It's been a while!"
+    end
+  # test "the truth" do
+  #   assert true
+  # end
+end
