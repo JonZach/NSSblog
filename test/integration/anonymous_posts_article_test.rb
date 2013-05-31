23c904f340a6a99bfdf286e958f40565995697ae
JonZach <Zach@yourgreenpal.com>
2013-05-31 12:18:34 -0500

    set up routes, updated post controller, and post tests

--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -4,12 +4,13 @@ class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
     test "view a list of articles" do
         Post.create!(title: "First Post!!", body: "This is awkward...I have nothing to say :(")
         Post.create!(title: "Wow, It's been a while!", body: "And yet, I'm still not a write :( :( lolol")
-        visit '/'
-        click_link "View All Posts"
-        assert_current_path '/posts'
+        # visit '/'
+        # click_link "View All Posts"
+        visit '/posts'
         # assume we have a page variable
-        assert_include page.content, "First Post!!"
-        assert_include page.content, "Wow, It's been a while!"
+        # assert_include page.content, "First Post!!"
+        assert_include page.body, "First Post!!"
+        assert_include page.body, "Wow, It's been a while!"
     end
   # test "the truth" do
   #   assert true
