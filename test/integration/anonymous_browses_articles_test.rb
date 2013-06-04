ec0a088eefdb55de2ae404064c2273b1c5080b5e
JonZach <Zach@yourgreenpal.com>
2013-06-04 11:58:47 -0500

    created path for article create

--- test/integration/anonymous_browses_articles_test.rb
+++ test/integration/anonymous_browses_articles_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class AnonymousBrowsesArticlesTest < ActionDispatch::IntegrationTest
     test "view a list of articles" do
         Post.create!(title: "First Post!!", body: "This is awkward...I have nothing to say :(")
-        Post.create!(title: "Wow, It's been a while!", body: "And yet, I'm still not a write :( :( lolol")
+        Post.create!(title: "Wow, It has been a while!", body: "And yet, I'm still not a write :( :( lolol")
         visit '/posts'
         assert_equal 2, Post.count
         assert_include page.body, "First Post!!"
-        assert_include page.body, "Wow, It's been a while!"
+        assert_include page.body, "Wow, It has been a while!"
     end
 end
